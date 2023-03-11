bcd389cef3181fa328b3e809b1a8c15f6c457347
SaishJ <saishjagtap0003@gmail.com>
2023-03-11 13:08:06 +0530

    :rewind: Stack Navigator
    Stack Navigator

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const Home = () => (
+  <View>
+    <Text>Home</Text>
+  </View>
+);
+
+const Detail = () => (
+  <View>
+    <Text>Detail</Text>
+  </View>
+);
+
+const Stack = createNativeStackNavigator();
+
+const Navigator = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="Detail" component={Detail} />
+    </Stack.Navigator>
+  );
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Navigator />
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
--- Readme.md
+++ Readme.md
@@ -9,6 +9,15 @@ npm install @react-navigation/native
 ```
 
 - In React Navigation have 3 types of Navigators
+
   - Stack Navigator: From one screen to another
   - Tab Navigator: For implementing tabs
   - Drawer Navigator: For implementing drawer i.e. pull from left side of screen.
+
+- NavigationContainer
+
+  > `NavigationContainer` is a component which manages our navigation tree and contains the navigation state. This component must wrap all navigators structure.
+
+- createNativeStackNavigator
+
+  > `createNativeStackNavigator` is a function that returns an object containing 2 properties: `Screen` and `Navigator`. Both of them are React components used for configuring the navigator. The `Navigator` should contain `Screen` elements as its children to define the configuration for routes.
